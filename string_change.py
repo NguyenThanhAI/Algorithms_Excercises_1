155b7e98d24b6028945b8824a24ee75d9e5d7ddb
NguyenThanhAI <Thanh.CEAA.K58.HUST@gmail.com>
2022-09-14 17:08:55 +0700

    Add tracing for example 1

--- string_change.py
+++ string_change.py
@@ -22,4 +22,39 @@ for i in range(len(orig_string) + 1):
             L[i, j] = min([L[i-1, j], L[i, j-1], L[i-1, j-1]]) + 1
 
 
+print(L[len(orig_string), len(target_string)])
+
 print(L)
+
+row = L.shape[0] - 1
+col = L.shape[1] - 1
+
+trace = []
+
+while row != 0 or L[row, col] != 0:
+    if L[row, col] == L[row, col-1] + 1:
+        trace.append("add")
+        col = col - 1
+    elif L[row, col] == L[row-1, col-1]:
+        trace.append("none")
+        row = row - 1
+        col = col - 1
+    elif L[row, col] == L[row - 1, col - 1] + 1:
+        trace.append("adjust")
+        row = row - 1
+        col = col -1
+    elif L[row, col] == L[row-1, col] + 1:
+        trace.append("remove")
+        row = row - 1
+
+#trace.reverse()
+    
+print(trace)
+
+trace = []
+
+'''def backtrack(row: int, col: int):
+
+    for delta_row, delta_col in [(-1, 0), (-1, -1), (0, -1)]:
+        if delta_row == -1 and delta_col == -1:
+            if'''
